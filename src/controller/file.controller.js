9137d9481e9ad77b7b7e246cc5988d4d5ebca491
rianpediaa <126028334+rianpediaa@users.noreply.github.com>
2023-04-08 17:04:57 +0700

    Update file.controller.js

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -55,7 +55,7 @@ const download = (req, res) => {
   const fileName = req.params.name;
   const directoryPath = __basedir + "/resources/static/assets/uploads/";
 
-  res.download(directoryPath + fileName, fileName, (err) => {
+  res.sendFile(directoryPath + fileName, fileName, (err) => {
     if (err) {
       res.status(500).send({
         message: "Could not download the file. " + err,
